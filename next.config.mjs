8ac329fa94d9d4ad2c7f15c85953f0827bed9b33
Dima <dimadeg2202@gmail.com>
2024-03-07 21:34:57 +0500

    Customized config

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	poweredByHeader: false, // чтобы если пробивали сайт не увидили что он сделан на next js
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:4200/api/:path*`
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `http://localhost:4200/uploads/:path*`
+			}
+		]
+	}
+}
+
+export default nextConfig
