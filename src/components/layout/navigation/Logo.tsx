dda4515f5270b0f70600a1be60ec38a4fe6211b9
Dima <dimadeg2202@gmail.com>
2024-03-09 22:35:05 +0500

    Logo

---
+++ src/components/layout/navigation/Logo.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+
+
+import logoImage from '@/assets/images/logo.png'
+
+const Logo: FC = () => {
+  return (
+    <Link href='/'>
+      <div className='px-layout mb-10 block'>
+        <Image src={logoImage} width={247} height={34} alt='Online cinema'/>
+      </div>
+    </Link>
+  )
+}
+
+export default Logo
