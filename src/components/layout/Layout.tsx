56d4b5d7c99aed5c16b42c3c0bbeb232c39b89a1
Dima <dimadeg2202@gmail.com>
2024-03-09 22:35:27 +0500

    Layout for common components

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react'
+
+import s from './Layout.module.scss'
+import Navigation from './navigation/Navigation'
+import SideBar from './sideBar/SideBar'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Layout: FC<Props> = ({ children }) => {
+	return (
+		<div className={s.layout}>
+			<Navigation />
+			<div className={s.center}>{children}</div>
+			<SideBar />
+		</div>
+	)
+}
+
+export default Layout
