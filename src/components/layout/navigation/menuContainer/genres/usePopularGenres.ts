3a090fed6e8bc25fd827de187a27bea66ee78b5a
Dima <dimadeg2202@gmail.com>
2024-03-09 22:32:59 +0500

    Hook to request popular genres

---
+++ src/components/layout/navigation/menuContainer/genres/usePopularGenres.ts
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getGenreUrl } from '@/config/url.config'
+
+import { GenreService } from '@/services/genre.service'
+
+import { IMenuItem } from '../menu.interface'
+
+export const usePopularGenres = () => {
+	const queryData = useQuery({
+		queryKey: ['popular genre menu'],
+		queryFn: () => GenreService.getPopularGenres(),
+		select: ({ data }) =>
+			data
+				.map(
+					genre =>
+						({
+							icon: genre.icon,
+							link: getGenreUrl(genre.slug),
+							title: genre.name
+						}) as IMenuItem
+				)
+				.splice(0, 4)
+	})
+
+	return queryData
+}
