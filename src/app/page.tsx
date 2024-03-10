dcc1b602a30ff9c4cdf384268d7a91e5a7265beb
Dima <dimadeg2202@gmail.com>
2024-03-10 20:00:13 +0500

    Added seo for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Home from '@/components/screens/home/Home'
-import { NextPage } from 'next'
+import { Metadata, NextPage } from 'next'
 
+import Home from '@/components/screens/home/Home'
+
+import { siteName, titleMerge } from '@/config/seo.config'
+
+export const metadata: Metadata = {
+	title: titleMerge('Watch movies online'),
+	description:
+		'Watch MovieApp movies and TV shows online or stream right to your browser',
+	twitter: {
+		card: 'summary_large_image'
+	},
+	openGraph: {
+		title: titleMerge('Watch movies online'),
+		description:
+			'Watch MovieApp movies and TV shows online or stream right to your browser',
+		url: 'localhost:3000',
+		siteName: siteName,
+		locale: 'en_US',
+		type: 'website'
+	}
+}
 
 const HomePage: NextPage = () => {
-  return <Home/>
+	return <Home />
 }
 
 export default HomePage
