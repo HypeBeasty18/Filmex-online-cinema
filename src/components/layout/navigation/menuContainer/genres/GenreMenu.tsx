66361c3d75bf2faa58ef13692f9323f26dca13de
Dima <dimadeg2202@gmail.com>
2024-03-10 19:59:54 +0500

    Added skeleton laoder

--- src/components/layout/navigation/menuContainer/genres/GenreMenu.tsx
+++ src/components/layout/navigation/menuContainer/genres/GenreMenu.tsx
@@ -5,12 +5,13 @@ import { FC } from 'react'
 import Menu from '../menu/Menu'
 
 import { usePopularGenres } from './usePopularGenres'
+import SkeletonLoader from '@/components/ui/SkeletonLoader'
 
 const GenreMenu: FC = () => {
 	const { isLoading, data } = usePopularGenres()
 
 	return isLoading ? (
-		<div className='mx-11 mb-6'>Loading...</div>
+		<div className='mx-11 mb-6'><SkeletonLoader count={5} className='h-7 mb-5'/></div>
 	) : (
 		<Menu menu={{ title: 'Popular genres', items: data || [] }} />
 	)
