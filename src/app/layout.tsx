6faf5382b8826d953f5320819b3bb86a1d696712
Dima <dimadeg2202@gmail.com>
2024-03-10 20:00:32 +0500

    Added default seo in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,25 @@
 import MainProvider from '@/app/MainProvider'
 import type { Metadata } from 'next'
 
+import { siteName, titleMerge } from '@/config/seo.config'
+
 import './globals.scss'
 
 export const metadata: Metadata = {
 	title: 'Filmex',
-	description: 'Online cinema | Filmex'
+	description: 'Online cinema | Filmex',
+	twitter: {
+		card: 'summary_large_image'
+	},
+	openGraph: {
+		title: titleMerge('Watch movies online'),
+		description:
+			'Watch MovieApp movies and TV shows online or stream right to your browser',
+		url: 'localhost:3000',
+		siteName: siteName,
+		locale: 'en_US',
+		type: 'website'
+	}
 }
 
 export default function RootLayout({
