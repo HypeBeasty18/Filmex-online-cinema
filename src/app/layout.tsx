4eb80bbf2ba6625598efb1f2ecc74d290ba4074a
Dima <dimadeg2202@gmail.com>
2024-03-10 15:42:22 +0500

    Added provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,23 @@
-import type { Metadata } from "next";
+import MainProvider from '@/app/MainProvider'
+import type { Metadata } from 'next'
+
 import './globals.scss'
 
-
 export const metadata: Metadata = {
-  title: "Filmex",
-  description: "Online cinema | Filmex",
-};
+	title: 'Filmex',
+	description: 'Online cinema | Filmex'
+}
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body >{children}</body>
-    </html>
-  );
+	return (
+		<MainProvider>
+			<html lang='en'>
+				<body>{children}</body>
+			</html>
+		</MainProvider>
+	)
 }
