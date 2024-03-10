20d29745082d1d8535cfb66de8f0beb587bcfe25
Dima <dimadeg2202@gmail.com>
2024-03-10 19:59:27 +0500

    Added heading

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,13 +1,20 @@
+import { Metadata } from 'next'
+import Head from 'next/head'
 import { FC } from 'react'
-import { IHome } from './home.interface'
+
 import Layout from '@/components/layout/Layout'
 
+import { IHome } from './home.interface'
+import Heading from '@/components/ui/heading/Heading'
+
+
+
 const Home: FC<IHome> = () => {
-  return (
-    <Layout>
-      <h1>Home</h1>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Heading title='Watch movies online' className='text-gray-300 mb-8 text-xl'/>
+		</Layout>
+	)
 }
 
 export default Home
