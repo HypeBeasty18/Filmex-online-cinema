1555afddb7903899fe0fec5ba80ffdd583296904
Dima <dimadeg2202@gmail.com>
2024-03-11 23:58:47 +0500

    Ui search field

---
+++ src/components/ui/searchField/SearchField.tsx
@@ -0,0 +1,36 @@
+import { FC, SetStateAction } from 'react'
+
+import MaterialIcon from '../MaterialIcon'
+
+import s from './SearchField.module.scss'
+
+interface ISearchField {
+	searchTerm: string
+	handleSearch: (event: React.ChangeEvent<HTMLInputElement>) => void
+	setIsShow: React.Dispatch<SetStateAction<boolean>>
+}
+
+const SearchField: FC<ISearchField> = ({
+	searchTerm,
+	handleSearch,
+	setIsShow
+}) => {
+	const handleClick = (event: React.MouseEvent<HTMLInputElement>) => {
+		event.preventDefault()
+		setIsShow(true)
+	}
+
+	return (
+		<div className={s.search}>
+			<MaterialIcon name='MdSearch' />
+			<input
+				placeholder='Search'
+				value={searchTerm}
+				onChange={handleSearch}
+				onClick={handleClick}
+			/>
+		</div>
+	)
+}
+
+export default SearchField
