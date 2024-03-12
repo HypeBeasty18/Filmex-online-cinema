e5268d406902facc9e117146ea60e777b959b337
Dima <dimadeg2202@gmail.com>
2024-03-13 00:06:49 +0500

    Notification container

---
+++ src/providers/ReactToastify.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { FC } from 'react'
+import { ToastContainer } from 'react-toastify'
+
+const ReactToastify: FC = () => {
+	return <ToastContainer autoClose={3000} position='top-right'/>
+}
+
+export default ReactToastify
