955fee94b087bd1d5627325bec7760437c62eb4d
Dima <dimadeg2202@gmail.com>
2024-03-13 00:06:22 +0500

    Little changes

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,18 +1,17 @@
-import { Metadata } from 'next'
-import Head from 'next/head'
 import { FC } from 'react'
 
 import Layout from '@/components/layout/Layout'
-
-import { IHome } from './home.interface'
 import Heading from '@/components/ui/heading/Heading'
 
-
+import { IHome } from './home.interface'
 
 const Home: FC<IHome> = () => {
 	return (
 		<Layout>
-			<Heading title='Watch movies online' className='text-gray-300 mb-8 text-xl'/>
+			<Heading
+				title='Watch movies online'
+				className='text-gray-300 mb-8 text-xl'
+			/>
 		</Layout>
 	)
 }
--- src/utils/movie/getGenresList.ts
+++ src/utils/movie/getGenresList.ts
@@ -1,3 +1,3 @@
 export const getGenreslist = (index: number, length: number, name: string) => {
-	return (index + 1 === length ? name : name + ', ' )
+  return index + 1 === length ? name : name + ',';
 }
