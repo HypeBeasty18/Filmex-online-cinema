990f7baf64c7b285b3393ea2f44ae1ad06e70235
Dima <dimadeg2202@gmail.com>
2024-03-12 21:43:04 +0500

    Added movies container

--- src/components/layout/sideBar/SideBar.tsx
+++ src/components/layout/sideBar/SideBar.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react'
 
 import s from './SideBar.module.scss'
 import Search from './search/Search'
+import MoviesContainer from './moviesContainer/MoviesContainer'
 
 const SideBar: FC = () => {
   return (
     <div className={s.sidebar}>
       <Search />
+      <MoviesContainer />
     </div>
   )
 }
