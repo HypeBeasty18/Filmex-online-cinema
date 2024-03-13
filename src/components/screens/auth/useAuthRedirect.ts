6430d899437929ad8526b583505e429b53deccaf
Dima <dimadeg2202@gmail.com>
2024-03-13 21:18:13 +0500

    Hook to redirect the user

---
+++ src/components/screens/auth/useAuthRedirect.ts
@@ -0,0 +1,21 @@
+'use client'
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+import { useAuth } from '@/hooks/useAuth'
+
+export const useAuthRedirect = () => {
+	const { user } = useAuth()
+
+	const { push } = useRouter()
+	const searchParams = useSearchParams()
+
+	const redirect = searchParams.get('') || '/'
+
+	useEffect(() => {
+		if (user) {
+			push(redirect)
+		}
+	}, [user, redirect, push])
+}
