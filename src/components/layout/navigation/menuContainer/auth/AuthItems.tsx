296a753f39a22f9829ea68e5fe30b341b95d5af2
Dima <dimadeg2202@gmail.com>
2024-03-13 21:22:55 +0500

    Contains actions components for auth and not auth users

--- src/components/layout/navigation/menuContainer/auth/AuthItems.tsx
+++ src/components/layout/navigation/menuContainer/auth/AuthItems.tsx
@@ -1,11 +1,51 @@
+'use client'
+
 import { FC } from 'react'
 
+import { getAdminHomeUrl } from '@/config/url.config'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import MenuItem from '../menu/menuItem/MenuItem'
+
+import LogoutButton from './LogoutButton'
+
 const AuthItems: FC = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+	const { user } = useAuth()
+
+	return (
+		<>
+			{user ? (
+				<>
+					<MenuItem
+						item={{
+							icon: 'MdSettings',
+							link: '/profile',
+							title: 'Profile'
+						}}
+					/>
+					{user.isAdmin && (
+						<MenuItem
+							item={{
+								icon: 'MdOutlineLock',
+								link: getAdminHomeUrl(),
+								title: 'Admin panel'
+							}}
+						/>
+					)}
+					<LogoutButton />
+				</>
+			) : (
+				<MenuItem
+					item={{
+						icon: 'MdLogin',
+						link: '/auth',
+						title: 'Login'
+					}}
+				/>
+			)}
+		</>
+	)
 }
 
 export default AuthItems
