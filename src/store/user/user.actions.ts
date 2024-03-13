fc311d19ff8c844f023966f65b3a4f1ed9f73d78
Dima <dimadeg2202@gmail.com>
2024-03-13 21:11:47 +0500

    Async user actions with use asyncthunk

---
+++ src/store/user/user.actions.ts
@@ -0,0 +1,57 @@
+import { errorCatch } from '@/api/api.helpers'
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
+
+import { AuthService } from '@/services/auth/auth.service'
+
+import { toastError } from '@/utils/toast-error'
+
+import { IAuthResponse, IEmailPassword } from './user.interface'
+
+export const register = createAsyncThunk<IAuthResponse, IEmailPassword>(
+	'auth/register',
+	async ({ email, password }, thunkApi) => {
+		try {
+			const response = await AuthService.register(email, password)
+			toast.success('Successful registration!')
+			return response.data
+		} catch (error) {
+			toastError(error)
+			return thunkApi.rejectWithValue(error)
+		}
+	}
+)
+
+export const login = createAsyncThunk<IAuthResponse, IEmailPassword>(
+	'auth/login',
+	async ({ email, password }, thunkApi) => {
+		try {
+			const response = await AuthService.login(email, password)
+			toast.success('Successful login!')
+			return response.data
+		} catch (error) {
+			toastError(error)
+			return thunkApi.rejectWithValue(error)
+		}
+	}
+)
+
+export const logout = createAsyncThunk('auth/logout', async (_, thunkApi) => {
+	await AuthService.logout()
+})
+
+export const checkAuth = createAsyncThunk<IAuthResponse, IEmailPassword>(
+	'auth/check-auth',
+	async (_, thunkApi) => {
+		try {
+			const response = await AuthService.geetNewsTokens()
+			return response.data
+		} catch (error) {
+			if (errorCatch(error) === 'jwt expired') {
+				toast.info('Your session has expired. Please login again.')
+				thunkApi.dispatch(logout())
+			}
+			return thunkApi.rejectWithValue(error)
+		}
+	}
+)
