d2783b5a6debaf1d61692f0cd4b728b1282f82ad
Dima <dimadeg2202@gmail.com>
2024-03-13 21:20:11 +0500

    Ui logout button

--- src/components/layout/navigation/menuContainer/auth/LogoutButton.tsx
+++ src/components/layout/navigation/menuContainer/auth/LogoutButton.tsx
@@ -1,9 +1,27 @@
 import { FC } from 'react'
 
+import MaterialIcon from '@/components/ui/MaterialIcon'
+
+import { useActions } from '@/hooks/useActions'
+
+import s from './LogoutButton.module.scss'
+
 const LogoutButton: FC = () => {
-  return (
-    
-  )
+	const { logout } = useActions()
+
+	const handleLogout = (e: React.MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault()
+		logout()
+	}
+
+	return (
+		<li className={s.li}>
+			<a onClick={handleLogout}>
+				<MaterialIcon name='MdLogout' />
+				<span>Logout</span>
+			</a>
+		</li>
+	)
 }
 
 export default LogoutButton
