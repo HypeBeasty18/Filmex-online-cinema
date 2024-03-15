8722f2bf961ca5839763661f64dfcbf9f15eca25
Dima <dimadeg2202@gmail.com>
2024-03-15 19:13:49 +0500

    Fixed auth items in navigation panel

--- src/components/layout/navigation/menuContainer/auth/AuthItems.tsx
+++ src/components/layout/navigation/menuContainer/auth/AuthItems.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC } from 'react'
+import { FC, Suspense, useEffect, useState } from 'react'
 
 import { getAdminHomeUrl } from '@/config/url.config'
 
@@ -13,37 +13,45 @@ import LogoutButton from './LogoutButton'
 const AuthItems: FC = () => {
 	const { user } = useAuth()
 
+	const [isClient, setIsClient] = useState(false)
+
+	useEffect(() => {
+		setIsClient(true)
+	}, [])
+
 	return (
 		<>
-			{user ? (
-				<>
-					<MenuItem
-						item={{
-							icon: 'MdSettings',
-							link: '/profile',
-							title: 'Profile'
-						}}
-					/>
-					{user.isAdmin && (
+			{isClient ? (
+				user ? (
+					<>
 						<MenuItem
 							item={{
-								icon: 'MdOutlineLock',
-								link: getAdminHomeUrl(),
-								title: 'Admin panel'
+								icon: 'MdSettings',
+								link: '/profile',
+								title: 'Profile'
 							}}
 						/>
-					)}
-					<LogoutButton />
-				</>
-			) : (
-				<MenuItem
-					item={{
-						icon: 'MdLogin',
-						link: '/auth',
-						title: 'Login'
-					}}
-				/>
-			)}
+						{user.isAdmin && (
+							<MenuItem
+								item={{
+									icon: 'MdOutlineLock',
+									link: getAdminHomeUrl(),
+									title: 'Admin panel'
+								}}
+							/>
+						)}
+						<LogoutButton />
+					</>
+				) : (
+					<MenuItem
+						item={{
+							icon: 'MdLogin',
+							link: '/auth',
+							title: 'Login'
+						}}
+					/>
+				)
+			) : null}
 		</>
 	)
 }
