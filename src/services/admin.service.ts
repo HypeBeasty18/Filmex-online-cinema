55a8981b853089d302ba7a8792268a78b331ce32
Dima <dimadeg2202@gmail.com>
2024-03-15 19:09:33 +0500

    Admin service

---
+++ src/services/admin.service.ts
@@ -0,0 +1,9 @@
+import { instance } from "@/api/interceptors"
+import { getUsersUrl } from "@/config/api.config"
+
+
+export const AdminService = {
+  async getCountUsers() {
+    return instance.get<number>(getUsersUrl('/count'))
+  } 
+}
