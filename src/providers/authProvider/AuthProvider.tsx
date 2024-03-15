112e909b7b768245946e927fa3b2f7b9dd778fc9
Dima <dimadeg2202@gmail.com>
2024-03-15 19:10:37 +0500

    Auth provider

---
+++ src/providers/authProvider/AuthProvider.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Cookies from 'js-cookie'
+import dynamic from 'next/dynamic'
+import { usePathname } from 'next/navigation'
+import { Component, FC, useEffect } from 'react'
+
+import { useActions } from '@/hooks/useActions'
+import { useAuth } from '@/hooks/useAuth'
+
+import { TypeComponentAuthFields } from '@/shared/types/auth.types'
+
+const DynamicCheckRole = dynamic(() => import('./CheckRoles'), {
+	ssr: false
+})
+
+const AuthProvider: FC<TypeComponentAuthFields> = ({
+	children,
+	Component: { isOnlyAdmin, isOnlyUser }
+}) => {
+	const { user } = useAuth()
+	const { logout, checkAuth } = useActions()
+
+	const pathname = usePathname()
+
+	useEffect(() => {
+		const accessToken = Cookies.get('accessToken')
+		if (accessToken) checkAuth()
+	}, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+	useEffect(() => {
+		const refreshToken = Cookies.get('refreshToken')
+		if (!refreshToken && user) logout()
+	}, [pathname]) // eslint-disable-line react-hooks/exhaustive-deps
+
+	return !isOnlyAdmin && !isOnlyUser ? (
+		<>{children}</>
+	) : (
+		<DynamicCheckRole Component={{ isOnlyAdmin, isOnlyUser }}>
+			{children}
+		</DynamicCheckRole>
+	)
+}
+
+export default AuthProvider
