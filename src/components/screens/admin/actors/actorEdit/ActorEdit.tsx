501f51f43267b0b175255d2518d96d863da9696b
Dima <dimadeg2202@gmail.com>
2024-03-16 21:50:08 +0500

    Added controllers to upload image

--- src/components/screens/admin/actors/actorEdit/ActorEdit.tsx
+++ src/components/screens/admin/actors/actorEdit/ActorEdit.tsx
@@ -2,14 +2,13 @@
 
 import { FC } from 'react'
 import { Controller, useForm } from 'react-hook-form'
-import { stripHtml } from 'string-strip-html'
 
 import Layout from '@/components/layout/Layout'
 import SkeletonLoader from '@/components/ui/SkeletonLoader'
 import Button from '@/components/ui/form-elements/Button'
 import Field from '@/components/ui/form-elements/Field'
-import TextEditor from '@/components/ui/form-elements/TextEditor'
 import SlugField from '@/components/ui/form-elements/slugField/SlugField'
+import UploadField from '@/components/ui/form-elements/uploadField/UploadField'
 import Heading from '@/components/ui/heading/Heading'
 
 import generateSlug from '@/utils/string/generateSlug'
@@ -62,24 +61,26 @@ const ActorEdit: FC = () => {
 								/>
 							</div>
 
-							{/* <Controller
-							control={control}
-							name='photo'
-							defaultValue=''
-							render={({
-								field: { value, onChange },
-								fieldState: { error }
-							}) => (
-
-							)}
-							rules={{
-								validate: {
-									required: v =>
-										(v && stripHtml(v).result.length > 0) ||
-										'Photo is required'
-								}
-							}}
-						/> */}
+							<Controller
+								control={control}
+								name='photo'
+								defaultValue=''
+								render={({
+									field: { value, onChange },
+									fieldState: { error }
+								}) => (
+									<UploadField
+										onChange={onChange}
+										value={value}
+										error={error?.message?.toString()}
+										folder='actors'
+										placeholder='Photo'
+									/>
+								)}
+								rules={{
+									required: 'Photo is required'
+								}}
+							/>
 						</div>
 
 						<Button>Update</Button>
