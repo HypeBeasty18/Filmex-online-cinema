5b936c59669541200fe3d02bd3d05d4405101775
Dima <dimadeg2202@gmail.com>
2024-03-16 21:49:47 +0500

    Added controllers to upload image and video

--- src/components/screens/admin/movies/movieEdit/MovieEdit.tsx
+++ src/components/screens/admin/movies/movieEdit/MovieEdit.tsx
@@ -2,24 +2,26 @@
 
 import { FC } from 'react'
 import { Controller, useForm } from 'react-hook-form'
-import { stripHtml } from 'string-strip-html'
 
 import Layout from '@/components/layout/Layout'
 import SkeletonLoader from '@/components/ui/SkeletonLoader'
 import Button from '@/components/ui/form-elements/Button'
 import Field from '@/components/ui/form-elements/Field'
-import TextEditor from '@/components/ui/form-elements/TextEditor'
 import SlugField from '@/components/ui/form-elements/slugField/SlugField'
+import UploadField from '@/components/ui/form-elements/uploadField/UploadField'
 import Heading from '@/components/ui/heading/Heading'
+import Select from '@/components/ui/select/Select'
 
 import generateSlug from '@/utils/string/generateSlug'
 
 import formStyles from '../../../../ui/form-elements/admin-form.module.scss'
+import { useAdminActors } from '../../genres/genreEdit/useAdminActors'
+import { useAdminGenre } from '../../genres/genreEdit/useAdminGenre'
 
 import { IMovieEditInput } from './movie-edit.interface'
 import { useMovieEdit } from './useMovieEdit'
 
-const MovieEdit: FC = () => {
+const ActorEdit: FC = () => {
 	const {
 		register,
 		handleSubmit,
@@ -33,8 +35,11 @@ const MovieEdit: FC = () => {
 
 	const { isLoading, onSubmit } = useMovieEdit(setValue)
 
+	const { isLoading: isActorsLoading, data: dataActors } = useAdminActors()
+	const { isLoading: isGenresLoading, data: dataGenres } = useAdminGenre()
+
 	const handleGenerate = () => {
-		setValue('slug', generateSlug(getValues('name')))
+		setValue('slug', generateSlug(getValues('title')))
 	}
 
 	return (
@@ -47,15 +52,14 @@ const MovieEdit: FC = () => {
 					<>
 						<div className={formStyles.fields}>
 							<Field
-								{...register('name', {
-									required: 'Name is required'
+								{...register('title', {
+									required: 'Title is required'
 								})}
-								placeholder='Name'
-								error={errors.name?.message?.toString()}
-								style={{ width: '31%' }}
+								placeholder='Title'
+								error={errors.title?.message?.toString()}
+								style={{ width: '48%' }}
 							/>
-
-							<div style={{ width: '31%' }}>
+							<div style={{ width: '48%' }}>
 								<SlugField
 									register={register}
 									error={errors.slug}
@@ -64,38 +68,141 @@ const MovieEdit: FC = () => {
 							</div>
 
 							<Field
-								{...register('icon', {
-									required: 'Icon is required'
+								{...register('parameters.country', {
+									required: 'Country is required'
 								})}
-								placeholder='Icon'
-								error={errors.icon?.message?.toString()}
+								placeholder='Country'
+								error={errors.parameters?.country?.message?.toString()}
 								style={{ width: '31%' }}
 							/>
-						</div>
+							<Field
+								{...register('parameters.duration', {
+									required: 'Duration is required'
+								})}
+								placeholder='Duration (min.)'
+								error={errors.parameters?.duration?.message?.toString()}
+								style={{ width: '31%' }}
+							/>
+							<Field
+								{...register('parameters.year', {
+									required: 'Year is required'
+								})}
+								placeholder='Year'
+								error={errors.parameters?.year?.message?.toString()}
+								style={{ width: '31%' }}
+							/>
 
-						<Controller
-							control={control}
-							name='description'
-							defaultValue=''
-							render={({
-								field: { value, onChange },
-								fieldState: { error }
-							}) => (
-								<TextEditor
-									placeholder='Description'
-									onChange={onChange}
-									value={value}
-									error={error?.message?.toString()}
+							<div style={{ width: '48%' }}>
+								{' '}
+								<Controller
+									control={control}
+									name='genres'
+									render={({ field, fieldState: { error } }) => (
+										<Select
+											field={field}
+											options={dataGenres || []}
+											isloading={isGenresLoading}
+											isMulti
+											placeholder='Genres'
+											error={error?.message?.toString()}
+										/>
+									)}
+									rules={{
+										required: 'Plese select at least one genre'
+									}}
 								/>
-							)}
-							rules={{
-								validate: {
-									required: v =>
-										(v && stripHtml(v).result.length > 0) ||
-										'Description is required'
-								}
-							}}
-						/>
+							</div>
+
+							<div style={{ width: '48%' }}>
+								<Controller
+									control={control}
+									name='actors'
+									render={({ field, fieldState: { error } }) => (
+										<Select
+											field={field}
+											options={dataActors || []}
+											isloading={isActorsLoading}
+											isMulti
+											placeholder='Actors'
+											error={error?.message?.toString()}
+										/>
+									)}
+									rules={{
+										required: 'Plese select at least one actor'
+									}}
+								/>
+							</div>
+							<div style={{ width: '48%' }}>
+								<Controller
+									control={control}
+									name='poster'
+									defaultValue=''
+									render={({
+										field: { value, onChange },
+										fieldState: { error }
+									}) => (
+										<UploadField
+											onChange={onChange}
+											value={value}
+											error={error?.message?.toString()}
+											folder='movies'
+											placeholder='Poster'
+										/>
+									)}
+									rules={{
+										required: 'Poster is required'
+									}}
+								/>
+							</div>
+
+							<div style={{ width: '48%' }}>
+								<Controller
+									control={control}
+									name='bigPoster'
+									defaultValue=''
+									render={({
+										field: { value, onChange },
+										fieldState: { error }
+									}) => (
+										<UploadField
+											onChange={onChange}
+											value={value}
+											error={error?.message?.toString()}
+											folder='movies'
+											placeholder='Big poster'
+										/>
+									)}
+									rules={{
+										required: 'Big poster is required'
+									}}
+								/>
+							</div>
+
+							<div style={{ width: '48%' }}>
+								<Controller
+									control={control}
+									name='videoUrl'
+									defaultValue=''
+									render={({
+										field: { value, onChange },
+										fieldState: { error }
+									}) => (
+										<UploadField
+											onChange={onChange}
+											value={value}
+											error={error?.message?.toString()}
+											folder='movies'
+											placeholder='Video'
+											style={{ marginTop: -25 }}
+											isNoImage
+										/>
+									)}
+									rules={{
+										required: 'Video is required'
+									}}
+								/>
+							</div>
+						</div>
 
 						<Button>Update</Button>
 					</>
@@ -105,4 +212,4 @@ const MovieEdit: FC = () => {
 	)
 }
 
-export default MovieEdit
+export default ActorEdit
