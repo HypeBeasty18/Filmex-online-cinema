48000b8b2843f4f115977e3172f0022ffd06ae4b
Dima <dimadeg2202@gmail.com>
2024-03-16 12:47:39 +0500

    Added output genres func

--- src/utils/movie/getGenresList.ts
+++ src/utils/movie/getGenresList.ts
@@ -1,3 +1,11 @@
 export const getGenreslist = (index: number, length: number, name: string) => {
-  return index + 1 === length ? name : name + ',';
+	return index + 1 === length ? name : name + ','
+}
+
+interface IArrayItem {
+	name: string
+}
+
+export const getGenresLists = (array: IArrayItem[]) => {
+	return array.map(item => item.name).join(', ')
 }
