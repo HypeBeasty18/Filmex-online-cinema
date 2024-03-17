dc03bc26d6e978d42b246f816fb10098c61ddc8b
Dima <dimadeg2202@gmail.com>
2024-03-17 18:14:47 +0500

    Ui component for slider

---
+++ src/components/ui/slider/SlideItem.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { FC } from 'react'
+
+import s from './Slider.module.scss'
+import { ISlide } from './slider.interface'
+
+interface ISlideItem {
+	slide: ISlide
+	buttonTitle?: string
+}
+
+const SlideItem: FC<ISlideItem> = ({ slide, buttonTitle = 'Watch' }) => {
+	const { push } = useRouter()
+
+	const handleClick = () => {
+		push(slide.link)
+	}
+
+	return (
+		<>
+			<div className={s.slide}>
+				{slide.bigPoster && (
+					<Image
+						src={slide.bigPoster}
+						alt={slide.title}
+						layout='fill'
+						className={s.image}
+						draggable={false}
+						priority
+						unoptimized
+					/>
+				)}
+				<div className={s.content}>
+					<div className={s.heading}>{slide.title}</div>
+					<div className={s.subHeading}>{slide.subTitle}</div>
+					<button className={s.button} onClick={handleClick}>
+						{buttonTitle}
+					</button>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default SlideItem
