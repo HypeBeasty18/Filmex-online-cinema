584169eb1ebc5337e7870e91e8eee5d873010609
Dima <dimadeg2202@gmail.com>
2024-03-17 18:15:33 +0500

    Added slider

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -2,16 +2,19 @@ import { FC } from 'react'
 
 import Layout from '@/components/layout/Layout'
 import Heading from '@/components/ui/heading/Heading'
+import Slider from '@/components/ui/slider/Slider'
 
 import { IHome } from './home.interface'
 
-const Home: FC<IHome> = () => {
+const Home: FC<IHome> = ({ slides }) => {
 	return (
 		<Layout>
 			<Heading
 				title='Watch movies online'
 				className='text-gray-300 mb-8 text-xl'
 			/>
+
+			{slides.length && <Slider slides={slides} />}
 		</Layout>
 	)
 }
