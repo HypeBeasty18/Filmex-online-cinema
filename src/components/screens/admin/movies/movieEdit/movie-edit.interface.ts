e21aacf67453468bf28f16b2f5959f522b056543
Dima <dimadeg2202@gmail.com>
2024-03-17 18:18:49 +0500

    Movie edit interface

--- src/components/screens/admin/movies/movieEdit/movie-edit.interface.ts
+++ src/components/screens/admin/movies/movieEdit/movie-edit.interface.ts
@@ -1,3 +1,8 @@
 import { IMovie } from '@/shared/types/movie.types'
 
-export interface IMovieEditInput extends Omit<IMovie, '_id'> {}
+export interface IMovieEditInput
+	extends Omit<IMovie, '_id' | 'rating' | 'countOpened' | 'genres' | 'actors'> {
+	genres: string[]
+	actors: string[]
+	description: string
+}
