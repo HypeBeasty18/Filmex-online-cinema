a750df3ea4b2275b39b924a64d4e7cf52b7fedd6
Dima <dimadeg2202@gmail.com>
2024-03-17 18:12:21 +0500

    Added function to receive data for slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { Metadata, NextPage } from 'next'
 
 import Home from '@/components/screens/home/Home'
+import { IHome } from '@/components/screens/home/home.interface'
+import { ISlide } from '@/components/ui/slider/slider.interface'
 
 import { siteName, titleMerge } from '@/config/seo.config'
+import { getMovieUrl } from '@/config/url.config'
+
+import { MovieService } from '@/services/movie.service'
+
+import { getGenresLists } from '@/utils/movie/getGenresList'
 
 export const metadata: Metadata = {
 	title: titleMerge('Watch movies online'),
@@ -22,8 +29,28 @@ export const metadata: Metadata = {
 	}
 }
 
-const HomePage: NextPage = () => {
-	return <Home />
+export const getData = async () => {
+	try {
+		const { data: movies } = await MovieService.getAll()
+
+		const slides: ISlide[] = movies.slice(0, 3).map(movie => ({
+			_id: movie._id,
+			link: getMovieUrl(movie.slug),
+			bigPoster: movie.bigPoster,
+			subTitle: getGenresLists(movie.genres),
+			title: movie.title
+		}))
+
+		return slides
+	} catch (error) {
+		return []
+	}
 }
 
-export default HomePage
+const page: NextPage<IHome> = async () => {
+	const slides = await getData()
+
+	return <Home slides={slides} />
+}
+
+export default page
