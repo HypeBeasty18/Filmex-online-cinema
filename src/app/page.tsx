5126211ca0dfc70788a5100daab3ad85f2f9077f
Dima <dimadeg2202@gmail.com>
2024-03-18 23:28:48 +0500

    Added get props to trending movies and actora

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import { getContentType } from '@/api/api.helpers'
-import { Metadata, NextPage } from 'next'
+import { Metadata } from 'next'
 
 import Home from '@/components/screens/home/Home'
-import { ISlide } from '@/components/ui/slider/slider.interface'
 
-import { API_URL } from '@/config/api.config'
 import { siteName, titleMerge } from '@/config/seo.config'
-import { getMovieUrl } from '@/config/url.config'
 
-import { IMovie } from '@/shared/types/movie.types'
-
-import { getGenresLists } from '@/utils/movie/getGenresList'
+import { ActorService } from '@/services/actor.service'
+import { MovieService } from '@/services/movie.service'
 
 export const metadata: Metadata = {
 	title: titleMerge('Watch movies online'),
@@ -30,32 +25,14 @@ export const metadata: Metadata = {
 	}
 }
 
-const getData = async () => {
-	try {
-		const response = await fetch(`${API_URL}/movies`, {
-			cache: 'force-cache',
-			headers: getContentType()
-		}).then(res => res.json())
-
-		const movies: IMovie[] = response
-
-		const slides: ISlide[] = movies.slice(0, 3).map(movie => ({
-			_id: movie._id,
-			link: getMovieUrl(movie.slug),
-			bigPoster: movie.bigPoster,
-			subTitle: getGenresLists(movie.genres),
-			title: movie.title
-		}))
-
-		return slides
-	} catch (error) {
-		return []
-	}
-}
-
 const page = async () => {
-	const slides = await getData()
-	return <Home slides={slides} />
+	const slides = await MovieService.fetchMovies()
+	const actors = await ActorService.fetchActors()
+	const trendingMovies = await MovieService.fetchTrendingMovies()
+
+	return (
+		<Home slides={slides} actors={actors} trendingMovies={trendingMovies} />
+	)
 }
 
 export default page
