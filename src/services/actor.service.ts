3e6e2ff0acbdd1935d4176de408d42d97cc7bd91
Dima <dimadeg2202@gmail.com>
2024-03-18 23:29:58 +0500

    Fetch request to actors

--- src/services/actor.service.ts
+++ src/services/actor.service.ts
@@ -1,11 +1,15 @@
+import { getContentType } from '@/api/api.helpers'
 import { instance } from '@/api/interceptors'
 
 import { IActorEditInput } from '@/components/screens/admin/actors/actorEdit/actor-edit.interface'
+import { IGalleryItem } from '@/components/ui/gallery/gallery.interface'
 
-import { getActorsUrl } from '@/config/api.config'
+import { API_URL, getActorsUrl } from '@/config/api.config'
 import { getActorUrl } from '@/config/url.config'
 
 import { IActor } from '@/shared/types/movie.types'
+
+import { transferToGalleryActors } from './home.helpers'
 
 export const ActorService = {
 	async getAll(searchTerm?: string) {
@@ -27,5 +31,22 @@ export const ActorService = {
 			getActorsUrl(`/${_id}`)
 		)
 		return response.data
+	},
+
+	async fetchActors() {
+		try {
+			const response = await fetch(`${API_URL}${getActorsUrl('')}`, {
+				cache: 'force-cache',
+				headers: getContentType()
+			}).then(res => res.json())
+
+			const actors: IActor[] = response
+
+			const actorsGallery: IGalleryItem[] = transferToGalleryActors(actors)
+
+			return actorsGallery
+		} catch (error) {
+			return []
+		}
 	}
 }
