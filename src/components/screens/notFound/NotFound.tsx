dc257c3b05c2cc6cda807fafc98aff8421fc4a0f
Dima <dimadeg2202@gmail.com>
2024-03-20 13:02:57 +0500

    Not found page (404 error)

---
+++ src/components/screens/notFound/NotFound.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+import Layout from '@/components/layout/Layout'
+import Heading from '@/components/ui/heading/Heading'
+
+const NotFound: FC = () => {
+	return (
+		<Layout>
+			<Heading title='404 - Page Not Found' />
+		</Layout>
+	)
+}
+
+export default NotFound
