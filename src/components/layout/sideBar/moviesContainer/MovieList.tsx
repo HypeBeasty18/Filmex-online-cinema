2bc53d33b461dd3af808916cbfdf73cb6df6b6b9
Dima <dimadeg2202@gmail.com>
2024-03-20 22:47:39 +0500

    Added some features

--- src/components/layout/sideBar/moviesContainer/MovieList.tsx
+++ src/components/layout/sideBar/moviesContainer/MovieList.tsx
@@ -5,18 +5,21 @@ import MovieItem from './MovieItem'
 import s from './Movielist.module.scss'
 import { IMovieList } from './movie-list.interface'
 
-const MovieList: FC<IMovieList> = ({ link, title, movies }) => {
-	return (
+const MovieList: FC<IMovieList> = ({ link, title, movies, many = false }) => {
+	return movies.length ? (
 		<div className={s.list}>
 			<div className={s.heading}>{title}</div>
+
 			{movies.map(movie => (
 				<MovieItem key={movie._id} movie={movie} />
 			))}
-			<Link href={link}>
-				<div className={s.button}>See more</div>
-			</Link>
+			{many && (
+				<Link href={link}>
+					<div className={s.button}>See more</div>
+				</Link>
+			)}
 		</div>
-	)
+	) : null
 }
 
 export default MovieList
