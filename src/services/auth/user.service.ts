46bd009ff432e2b4ecfcda0709a5049c45ce3182
Dima <dimadeg2202@gmail.com>
2024-03-20 14:24:22 +0500

    Added functions to interact with user's data

--- src/services/auth/user.service.ts
+++ src/services/auth/user.service.ts
@@ -1,16 +1,31 @@
 import { instance } from '@/api/interceptors'
 
+import { IProfileInput } from '@/components/screens/profile/profile.interface'
+
 import { getUsersUrl } from '@/config/api.config'
 
 import { IUser } from '@/shared/types/user.types'
 
 export const userService = {
 	async getAll(searchTerm: string) {
-		return instance.get<IUser[]>(getUsersUrl(''), {
+		return await instance.get<IUser[]>(getUsersUrl(''), {
 			params: searchTerm ? { searchTerm } : {}
 		})
 	},
 	async deleteUser(id: string) {
-		return instance.delete<string>(getUsersUrl(`/${id}`))
+		return await instance.delete<string>(getUsersUrl(`/${id}`))
+	},
+	async getProfile() {
+		return await instance.get<IUser>(getUsersUrl('/profile'))
+	},
+	async updateProfile(data: IProfileInput) {
+		return await instance.put<string>(getUsersUrl('/profile'), data)
+	},
+	async getById(_id: string) {
+		const response = await instance.get<IUser>(getUsersUrl(`/${_id}`))
+		return response.data
+	},
+	async update(_id: string, data: IProfileInput) {
+		return await instance.put<string>(getUsersUrl(`/${_id}`), data)
 	}
 }
