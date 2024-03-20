7af80d117f49a35d4684f9fb29966ec0ff6b9dc3
Dima <dimadeg2202@gmail.com>
2024-03-20 13:03:33 +0500

    Trending now page

--- src/app/trending/page.tsx
+++ src/app/trending/page.tsx
@@ -1,10 +1,34 @@
-import Trending from '@/components/screens/trending/Trending'
-import { FC } from 'react'
+import { Metadata, NextPage } from 'next'
 
-const page: FC = () => {
-  return (
-    <Trending/>
-  )
+import Trending from '@/components/screens/trending/Trending'
+
+import { siteName, titleMerge } from '@/config/seo.config'
+
+import { MovieService } from '@/services/movie.service'
+
+const description =
+	'Trending movies in excellent quality and comfort: legal, safe, and fun.'
+
+export const metadata: Metadata = {
+	title: titleMerge('Trending movies'),
+	description: description,
+	twitter: {
+		card: 'summary_large_image'
+	},
+	openGraph: {
+		title: titleMerge('Trending movies'),
+		description: description,
+		url: 'localhost:3000',
+		siteName: siteName,
+		locale: 'en_US',
+		type: 'website'
+	}
+}
+
+const page: NextPage = async () => {
+	const movies = await MovieService.fetchTrendingNow()
+
+	return <Trending movies={movies} description={description} />
 }
 
 export default page
