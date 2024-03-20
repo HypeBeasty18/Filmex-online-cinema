b10df5515d14c8632f575aae7dff8ded2fce0fdc
Dima <dimadeg2202@gmail.com>
2024-03-20 20:55:15 +0500

    Hook for rating component

---
+++ src/components/screens/movie/rateMovie/useRateMovie.ts
@@ -0,0 +1,50 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+
+import { RatingService } from '@/services/rating.service'
+
+export const useRateMovie = (movieId: string) => {
+	const [rating, setRating] = useState<number>(0)
+
+	const [isSended, setIsSended] = useState<boolean>(false)
+
+	const { isSuccess, isError, refetch, data } = useQuery({
+		queryKey: ['Your movie rating', movieId],
+		queryFn: () => RatingService.getByUserMovie(movieId),
+		enabled: !!movieId
+	})
+
+	useEffect(() => {
+		if (isSuccess) {
+			setRating(data.data)
+		}
+	}, [isSuccess])
+
+	useEffect(() => {
+		if (isError) {
+			toast.error('Your movie rating has not been received')
+		}
+	}, [isError])
+
+	const { mutateAsync } = useMutation({
+		mutationKey: ['Update movie rating'],
+		mutationFn: (value: number) => RatingService.setRating(movieId, value),
+		onError: () => toast.error('Error to load movie rating'),
+		onSuccess: () => {
+			setIsSended(true)
+			refetch()
+
+			setTimeout(() => {
+				setIsSended(false)
+			}, 2400)
+		}
+	})
+
+	const handleClick = async (ratingValue: number) => {
+		setRating(ratingValue)
+		await mutateAsync(ratingValue)
+	}
+
+	return { isSended, rating, handleClick }
+}
