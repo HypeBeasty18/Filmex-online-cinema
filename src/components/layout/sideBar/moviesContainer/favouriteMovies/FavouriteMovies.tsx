00cb5155c958318cee807ef00d11522cf0831e1d
Dima <dimadeg2202@gmail.com>
2024-03-20 22:47:12 +0500

    Component to show favourite movies in sidebar

--- src/components/layout/sideBar/moviesContainer/favouriteMovies/FavouriteMovies.tsx
+++ src/components/layout/sideBar/moviesContainer/favouriteMovies/FavouriteMovies.tsx
@@ -1,7 +1,47 @@
-import { FC } from 'react'
+'use client'
+
+import { FC, useEffect, useState } from 'react'
+
+import { useFavourites } from '@/components/screens/favourites/useFavourites'
+import SkeletonLoader from '@/components/ui/SkeletonLoader'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import MovieList from '../MovieList'
+
+import NotAuthFavourites from './NotAuthFavourites'
 
 const FavouriteMovies: FC = () => {
-	return <div>FavouriteMovies</div>
+	const [isClient, setIsClient] = useState<boolean>(false)
+
+	const { favouriteMovies, isLoading } = useFavourites()
+
+	const { user } = useAuth()
+
+	useEffect(() => {
+		setIsClient(true)
+	}, [])
+
+	return (
+		<>
+			{isClient && !isLoading ? (
+				<>
+					{user ? (
+						<MovieList
+							many={favouriteMovies && favouriteMovies?.length > 3}
+							movies={favouriteMovies?.slice(0, 3) || []}
+							link='/favourites'
+							title='Favourites'
+						/>
+					) : (
+						<NotAuthFavourites />
+					)}
+				</>
+			) : (
+				<SkeletonLoader count={3} className='h-28 mb-4' />
+			)}
+		</>
+	)
 }
 
 export default FavouriteMovies
