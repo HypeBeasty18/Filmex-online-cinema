c00850398066452c109715afb5ec32b34c25d764
Dima <dimadeg2202@gmail.com>
2024-03-20 13:02:41 +0500

    Trending now page

--- src/components/screens/trending/Trending.tsx
+++ src/components/screens/trending/Trending.tsx
@@ -1,12 +1,23 @@
-import Layout from '@/components/layout/Layout'
 import { FC } from 'react'
 
-const Trending: FC = () => {
-  return (
-    <Layout>
-      <h1>Trending</h1>
-    </Layout>
-  )
+import Layout from '@/components/layout/Layout'
+import Catalog from '@/components/ui/catalog-movies/Catalog'
+
+import { IMovie } from '@/shared/types/movie.types'
+
+const Trending: FC<{ movies: IMovie[]; description: string }> = ({
+	movies,
+	description
+}) => {
+	return (
+		<Layout>
+			<Catalog
+				movies={movies || []}
+				title='Trending movies'
+				description={description}
+			/>
+		</Layout>
+	)
 }
 
 export default Trending
