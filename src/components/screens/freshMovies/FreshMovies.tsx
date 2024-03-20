e31b21030ddfbea1192dd7ff939aaf7f59a9d32b
Dima <dimadeg2202@gmail.com>
2024-03-20 13:03:15 +0500

    Fresh movies page

--- src/components/screens/freshMovies/FreshMovies.tsx
+++ src/components/screens/freshMovies/FreshMovies.tsx
@@ -1,12 +1,23 @@
-import Layout from '@/components/layout/Layout'
 import { FC } from 'react'
 
-const FreshMovies: FC = () => {
-  return (
-    <Layout>
-      <h1>FreshMovies</h1>
-    </Layout>
-  )
+import Layout from '@/components/layout/Layout'
+import Catalog from '@/components/ui/catalog-movies/Catalog'
+
+import { IMovie } from '@/shared/types/movie.types'
+
+const FreshMovies: FC<{ movies: IMovie[]; description: string }> = ({
+	movies,
+	description
+}) => {
+	return (
+		<Layout>
+			<Catalog
+				movies={movies || []}
+				title='Fresh movies'
+				description={description}
+			/>
+		</Layout>
+	)
 }
 
 export default FreshMovies
