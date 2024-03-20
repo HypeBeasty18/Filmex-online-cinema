2cb60dc95716869e0130bb2ab6972aae93f3ccc3
Dima <dimadeg2202@gmail.com>
2024-03-20 13:04:03 +0500

    Fresh movies page

--- src/app/fresh/page.tsx
+++ src/app/fresh/page.tsx
@@ -1,10 +1,34 @@
-import FreshMovies from '@/components/screens/freshMovies/FreshMovies'
-import { FC } from 'react'
+import { Metadata, NextPage } from 'next'
 
-const page: FC = () => {
-  return (
-    <FreshMovies/>
-  )
+import FreshMovies from '@/components/screens/freshMovies/FreshMovies'
+
+import { siteName, titleMerge } from '@/config/seo.config'
+
+import { MovieService } from '@/services/movie.service'
+
+const description =
+	'New novies and series in excellent quality: legal, safe, wihout ads'
+
+export const metadata: Metadata = {
+	title: titleMerge('Fresh movies'),
+	description: description,
+	twitter: {
+		card: 'summary_large_image'
+	},
+	openGraph: {
+		title: titleMerge('Fresh movies'),
+		description: description,
+		url: 'localhost:3000',
+		siteName: siteName,
+		locale: 'en_US',
+		type: 'website'
+	}
+}
+
+const page: NextPage = async () => {
+	const movies = await MovieService.fetchAllMovies()
+
+	return <FreshMovies movies={movies} description={description} />
 }
 
 export default page
