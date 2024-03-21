31fd11a9ad2c45995edd7ac09fba4e1884acb216
Dima <dimadeg2202@gmail.com>
2024-03-21 10:33:26 +0500

    Fix

--- src/app/genres/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next'
-
-import Discovery from '@/components/screens/discovery/Discovery'
-
-const page: NextPage = () => {
-	return <Discovery />
-}
-
-export default page
