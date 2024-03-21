403cb1a84de2b63cdfbb08fbe4c166120baacbdf
Dima <dimadeg2202@gmail.com>
2024-03-21 10:34:25 +0500

    Discovery component

--- src/components/screens/discovery/Discovery.tsx
+++ src/components/screens/discovery/Discovery.tsx
@@ -1,12 +1,45 @@
-import Layout from '@/components/layout/Layout'
 import { FC } from 'react'
 
-const Discovery: FC = () => {
-  return (
-    <Layout>
-      <h1>Hello</h1>
-    </Layout>
-  )
+import Layout from '@/components/layout/Layout'
+import Description from '@/components/ui/heading/Description'
+import Heading from '@/components/ui/heading/Heading'
+
+import s from './Discovery.module.scss'
+import DiscoveryItem from './DiscoveryItem'
+import { IDiscovery } from './discovery.interface'
+
+export interface IDiscoveryProps {
+	collections: IDiscovery[]
+	title: string
+	description: string
+}
+
+const Discovery: FC<IDiscoveryProps> = ({
+	collections,
+	description,
+	title
+}) => {
+	return (
+		<Layout>
+			<Heading title={title} />
+			<Description text={description} className={s.description} />
+			{collections.length ? (
+				<>
+					{collections.map(collect => (
+						<DiscoveryItem key={collect._id} collection={collect} />
+					))}
+				</>
+			) : (
+				<>
+					<h3 className='mt-10 text-white text-3xl text-center'>
+						Current page in progress...
+					</h3>
+				</>
+			)}
+
+			<section className={s.collections}></section>
+		</Layout>
+	)
 }
 
 export default Discovery
